6deb2b52d95367e1d938561e2c9d21fc4da1d09d
bincybabu <bincysoja2017@gmail.com>
2023-06-06 20:38:07 +0530

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\OrderProducts;
+
+class ProductController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $product = Product::with('cat_deatils')->orderBy('id','desc')->paginate(5);
+        return view('Product.index',compact('product'));
+    }
+
+     public function add_product()
+    {
+        $cat=Category::get();
+        return view('Product.add',compact('cat'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+                'name'   => 'required',
+                'image'  =>'required|mimes:jpeg,png,jpg,gif',
+                'cat_id' => 'required',
+                'price'  => 'required'
+        ]);
+        
+        
+        if($request->hasFile('image'))
+        {
+            $filename=$request->getSchemeAndHttpHost(). '/assets/image/' . time() . '.' .$request->image->extension();
+
+            $request->image->move(public_path('/assets/image/'),$filename);
+        }
+        $product=new Product();
+        $product->name=$request->name;
+        $product->cat_id=$request->cat_id;
+        $product->price=$request->price;
+        $product->image=$filename;
+        $product->save();
+
+        return redirect()->route('list_product')->with('success','Product has been created successfully.');
+    }
+
+    public function edit(Request $req)
+    {
+        $cat=Category::get();
+        $product=Product::where('id',$req->id)->first();
+        return view('Product.edit',compact('product','cat'));
+    }
+
+    public function update(Request $req)
+    {
+        $req->validate([
+                'name'   => 'required',
+                'cat_id' => 'required',
+                'price'  => 'required'
+        ]);
+        
+        
+        if($req->hasFile('image') != null)
+        {
+            $filename=$req->getSchemeAndHttpHost(). '/assets/image/' . time() . '.' .$req->image->extension();
+
+            $req->image->move(public_path('/assets/image/'),$filename);
+        }
+        else
+        {
+          $data= Product::where('id',$req->id)->first();
+          $filename=$data->image;
+        }
+        
+        $category=Product::where('id',$req->id)->update(['name'=>$req->name,'cat_id'=>$req->cat_id,'price'=>$req->price,'image'=>$filename]);
+
+        return redirect()->route('list_product')->with('success','Product Has Been updated successfully');
+    }
+
+        
+     public function destroy($id)
+    {
+        
+        $checkexists=OrderProducts::where('product',$id)->exists();
+        if($checkexists==true)
+        {
+          return redirect()->route('list_product')->with('success','Cannot delete Product!.');
+        }
+        else
+        {
+          Product::find($id)->delete();
+          return redirect()->route('list_product')->with('success','Product has been deleted successfully');
+        
+        }
+       
+    }
+}
---
+++ resources/views/Product/add.blade.php
@@ -0,0 +1,90 @@
+@extends('layouts.app')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Add Product</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left mb-2">
+                    <h2>Add Product</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('list_product') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+        @if(session('status'))
+        <div class="alert alert-success mb-1 mt-1">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form action="{{ route('product_store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Product Name:</strong>
+                        <input type="text" name="name" class="form-control" placeholder="Product Name" autocomplete="off">
+                        @error('name')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Product Image:</strong>
+                         <input type="file" name="image" class="form-control">
+                         @error('image')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                        
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Product Category:</strong>
+                        <select name="cat_id">
+                        @foreach($cat as $val)
+                        <option value='{{ $val->id }}'>{{ $val->name}}</option>
+                        @endforeach
+                    </select>
+                        @error('cat_id')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Product Price:</strong>
+                        <input type="number" name="price" class="form-control" placeholder="Product Price" min="1" autocomplete="off">
+                        @error('price')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+
+                
+                
+                
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
+
+@endsection
